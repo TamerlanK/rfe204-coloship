bcb4955a901bfa86ee44f5d30fa5b9fddaba7fa2
Tamerlan Kengerli <tamerlankengerli2002@gmail.com>
2024-08-03 05:06:57 +0400

    set tailwind variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,24 @@
 export default {
   content: ["./index.html", "./src/**/*.{ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: "#fe4c50",
+          light: "#fe7c7f",
+        },
+        ntrl: {
+          DEFAULT: "#1e1e27",
+          clr600: "##282828",
+          clr500: "#51545f",
+          clr400: "##b5aec4",
+          clr300: "##eceff6",
+          clr200: "#f3f3f3",
+          clr100: "#f2f2f2",
+        },
+        success: "#51a042",
+      },
+    },
   },
   plugins: [],
 }
